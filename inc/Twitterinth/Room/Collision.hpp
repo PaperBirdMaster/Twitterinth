0bda9a30e4d8bd6508c8f857deae39b3d0e5617d
PaperBirdMaster <PaperBirdMaster@DESKTOP-G5A5BMF>
2017-06-11 13:19:25 +0200

    Primera versión archivos de colisión en sala (Room).

---
+++ inc/Twitterinth/Room/Collision.hpp
@@ -0,0 +1,11 @@
+#ifndef TWITTERINTH_ROOM_COLLISION_HPP
+#define TWITTERINTH_ROOM_COLLISION_HPP
+
+namespace Twitterinth
+{
+
+// Place code here
+
+}
+
+#endif
---
+++ src/Twitterinth/Room/Collision.cpp
@@ -0,0 +1,9 @@
+// App includes.
+#include "Room/Collision.hpp"
+
+namespace Twitterinth
+{
+
+// place code here.
+
+}
